78abb5cd4b583049a64612c772f7cbb3973fa1c3
posseggs <simon.possegger@edu.htl-villach.at>
2019-04-01 09:55:43 +0200

    Copying from proto1 branch for further implementation

---
+++ MotionStream/app/src/main/java/com/posseggs/motionstream/SettingsActivity.java
@@ -0,0 +1,106 @@
+package com.posseggs.motionstream;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    EditText editUriText;
+    Switch editSwitchAuto;
+    Switch editSwitchPush;
+    Boolean autoplay = true; //Default on true
+    Boolean notify = true; //Default on true
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        editUriText = findViewById(R.id.editTextURI);
+        editSwitchAuto = findViewById(R.id.switchAutoplay);
+        editSwitchPush = findViewById(R.id.switchPushNotification);
+
+        //Set default values
+        editSwitchAuto.setChecked(autoplay);
+        editSwitchPush.setChecked(notify);
+
+        //Check if there already are valid settings
+        try
+        {
+            if (MainActivity.video != null)
+            {
+                if (MainActivity.video.getAutoplay() != null) {
+                    editSwitchAuto.setChecked(MainActivity.video.getAutoplay());
+                    autoplay = MainActivity.video.getAutoplay();
+                }
+                if (MainActivity.video.getNotify() != null)
+                {
+                    editSwitchPush.setChecked(MainActivity.video.getNotify());
+                    notify = MainActivity.video.getNotify();
+                }
+                if (MainActivity.video.getUri() != null && MainActivity.video.getUri().toString() != "")
+                    editUriText.setText(MainActivity.video.getUri().toString());
+            }
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this,"Error! No default settings exists!",Toast.LENGTH_LONG).show();
+        }
+
+        //Change Booleans when alternating switch
+        editSwitchAuto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    autoplay = true;
+                else
+                    autoplay = false;
+            }
+        });
+
+        editSwitchPush.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    notify = true;
+                else
+                    notify = false;
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_settings,menu);
+        return true;
+    }
+
+    public void apply_onClick(MenuItem menu)
+    {
+        try
+        {
+            //Apply settings
+            MainActivity.video.setUri(editUriText.getText().toString());
+            MainActivity.video.setAutoplay(autoplay);
+            MainActivity.video.setNotify(notify);
+            Intent i = new Intent();
+            setResult(RESULT_OK, i);
+            finish();
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, ex.getMessage(),Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void cancel_onClick(MenuItem menu)
+    {
+        //Chancel settings
+        Intent i = new Intent();
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+}
---
+++ MotionStream/app/src/main/java/com/posseggs/motionstream/Video.java
@@ -0,0 +1,79 @@
+package com.posseggs.motionstream;
+
+import android.net.Uri;
+
+public class Video {
+
+    private Uri uri;
+    private Boolean autoplay;
+    private Boolean notify;
+
+    public Video()
+    {
+        //Default uri
+        //setUri("rtmp://184.72.239.149/vod/mp4:bigbuckbunny_1500.mp4");
+    }
+
+    public Video(String uri, Boolean autoplay, Boolean notify)
+    {
+        try
+        {
+            setUri(uri);
+            setAutoplay(autoplay);
+            setNotify(notify);
+        }
+        catch (Exception ex)
+        {
+            throw new IllegalArgumentException("Error occurred: " + ex.getMessage());
+        }
+    }
+
+    public Uri getUri()
+    {
+        return uri;
+    }
+
+    public void setUri(String uri)
+    {
+        if (uri != "")
+        {
+            /*if (uri.matches("(^www\\.)?([A-Z]|[a-z])+([\\-\\.]{1}[a-z0-9]+)*[.]([a-z]){2,5}"))
+            { */
+                this.uri = Uri.parse(uri);
+            /*}
+            else
+            {
+                throw new IllegalArgumentException("Uri is invalid!");
+            }
+            */
+        }
+        else {
+            throw new IllegalArgumentException("Uri is empty!");
+        }
+    }
+
+    public Boolean getAutoplay()
+    {
+        return autoplay;
+    }
+
+    public void setAutoplay(Boolean autoplay)
+    {
+        if (autoplay == true || autoplay == false)
+            this.autoplay = autoplay;
+        else
+            throw new IllegalArgumentException("Invalid autoplay!");
+    }
+
+    public Boolean getNotify()
+    {
+        return notify;
+    }
+
+    public void setNotify(Boolean notify) {
+        if (notify == true || notify == false)
+            this.notify = notify;
+        else
+            throw new IllegalArgumentException("Invalid notify!");
+    }
+}
